f7152d44421cd5a4d8bd4a86a92fdc355239939c
almed anwar <doanthienan54@gmail.com>
2026-02-19 16:33:12 +0700

    2

--- manga/src/app/(app)/(home)/ranking/top-daily/page.tsx
+++ manga/src/app/(app)/(home)/ranking/top-daily/page.tsx
@@ -3,16 +3,22 @@
 import { NewUpdate } from "@/modules/home/ui/newUpdate";
 import { Mangas } from "@/payload-types";
 import { useTRPC } from "@/trpc/client";
-import {  useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 const Page = () => {
   const trpc = useTRPC();
 
-    const {data: manga} = useSuspenseQuery(trpc.magas.getRankDay.queryOptions({
-        page: 1
-    }))
-    const { data: category } = useSuspenseQuery(
-    trpc.category.getSubMany.queryOptions())
+  const { data: manga, isLoading } = useQuery(
+    trpc.magas.getRankDay.queryOptions({ page: 1 })
+  );
+
+  const { data: category } = useQuery(
+    trpc.category.getSubMany.queryOptions()
+  );
+
+  if (isLoading || !manga || !category) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="bg-popular">
--- manga/src/app/(app)/(home)/ranking/top-monthly/page.tsx
+++ manga/src/app/(app)/(home)/ranking/top-monthly/page.tsx
@@ -1,22 +1,25 @@
-"use client"
+"use client";
 
-import { NewUpdate } from "@/modules/home/ui/newUpdate"
-import { Mangas } from "@/payload-types"
-
-import { useTRPC } from "@/trpc/client"
-import { useSuspenseQuery } from "@tanstack/react-query"
-
-
+import { NewUpdate } from "@/modules/home/ui/newUpdate";
+import { Mangas } from "@/payload-types";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 const Page = () => {
-    const trpc = useTRPC()
-   
-    const {data: manga} = useSuspenseQuery(trpc.magas.getRankDay.queryOptions({
-        page: 1
-    }))
-    const { data: category } = useSuspenseQuery(
+  const trpc = useTRPC();
+
+  const { data: manga, isLoading } = useQuery(
+    trpc.magas.getRankMonth.queryOptions({ page: 1 })
+  );
+
+  const { data: category } = useQuery(
     trpc.category.getSubMany.queryOptions()
   );
+
+  if (isLoading || !manga || !category) {
+    return <div>Loading...</div>;
+  }
+
     return (
         <div className="bg-popular">
             <NewUpdate mangas={manga.docs as Mangas[]} category={category} page={manga.page} totalPages={manga.totalPages} />
