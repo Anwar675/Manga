9f43be8f74ae2256b54c90b7cf45200d5c085137
almed anwar <doanthienan54@gmail.com>
2026-02-20 09:44:35 +0700

    update

--- manga/src/app/(app)/(home)/manga/[manga]/page.tsx
+++ manga/src/app/(app)/(home)/manga/[manga]/page.tsx
@@ -5,7 +5,7 @@ import PageClient from "./pageClient";
 
 import { getImageUrl, richTextToPlainText } from "@/lib/seo";
 
-const SITE_URL = "https://alga.site";
+const SITE_URL = "https://alga21.site";
 
 export const revalidate = 60;
 
